7d25d9ca61d77f88736173ff7db85d6c836b5487
Soummya Biswas <soummyabiswas999@gmail.com>
2022-12-29 21:37:46 +0530

    chnages

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -11,58 +11,57 @@ const client = require('twilio')(
 
 
 
-
 exports.sendMessages = catchAsyncError(async (req, res) => {
   
   let name = req.body.name
   let to = req.body.to
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.NODEMAILER_EMAIL,
-          pass: process.env.NODEMAILER_PASSWORD
-        }
-      });
+
+  console.log("name& to", name , to)
+  //   const transporter = nodemailer.createTransport({
+  //       service: 'gmail',
+  //       auth: {
+  //         user: process.env.NODEMAILER_EMAIL,
+  //         pass: process.env.NODEMAILER_PASSWORD
+  //       }
+  //     });
       
 
-      const mailOptions = {
-        from: process.env.NODEMAILER_EMAIL,
-        to: to,
-        envelope: {
-          from: process.env.NODEMAILER_EMAIL,
-          to: to
-        },
-        subject: "shopgo.com",
-        html: `<b><p>Thank You ${name} , Your order will delivered withing 4-5 days . Thank you for ordering...</p></b>`
+  //     const mailOptions = {
+  //       from: process.env.NODEMAILER_EMAIL,
+  //       to: to,
+  //       envelope: {
+  //         from: process.env.NODEMAILER_EMAIL,
+  //         to: to
+  //       },
+  //       subject: "shopgo.com",
+  //       html: `<b><p>Thank You ${name} , Your order will delivered withing 4-5 days . Thank you for ordering...</p></b>`
 
-      };
+  //     };
 
 
-      return res.status(200).send({ success: true, mailOptions:mailOptions });
-
-    // transporter.sendMail(mailOptions, async function(error, info){
-    //     if (error) {
+  //   transporter.sendMail(mailOptions, async function(error, info){
+  //       if (error) {
         
-    //        return res.status(400).send({ success: false, error:error.stack });
-    //     } else {
-    //       client.messages
-    //       .create({
-    //         from: process.env.TWILIO_PHONE_NUMBER,
-    //         to: "+919874266014",
-    //         body: `${name} Order product ...please check`
-    //       })
-    //       .then(() => {
-    //          return res.status(200).send({ success: true,message:"Send Succesfully" });
+  //          return res.status(400).send({ success: false, error:error.stack });
+  //       } else {
+  //         client.messages
+  //         .create({
+  //           from: process.env.TWILIO_PHONE_NUMBER,
+  //           to: "+919874266014",
+  //           body: `${name} Order product ...please check`
+  //         })
+  //         .then(() => {
+  //            return res.status(200).send({ success: true,message:"Send Succesfully" });
           
-    //       })
-    //       .catch(err => {
-    //        console.log(error.stack)
-    //         return res.status(400).send({ success: false,error:err.stack });
+  //         })
+  //         .catch(err => {
+  //          console.log(error.stack)
+  //           return res.status(400).send({ success: false,error:err.stack });
             
-    //       });
+  //         });
       
-    //         return res.status(200).send({ success: true,info,message:"Send Succesfully" });
-    //     }
-    //   });
+  //           return res.status(200).send({ success: true,info,message:"Send Succesfully" });
+  //       }
+  //     });
 
 })
