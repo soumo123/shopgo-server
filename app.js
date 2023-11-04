cb4a4507dc9ea7d62aa3e171f7297d0cc179c8bb
Soummya Biswas <soummyabiswas999@gmail.com>
2023-11-04 20:07:51 +0530

    add crypto.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ require('./db/conn')
 
 
 // app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', `${process.env.REACT_CLIENT_URL}`);
+//     res.setHeader('Access-Control-Allow-Origin', `${process.env.REACT_LOCAL_CLIENT_URL}`);
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 //     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 //     res.setHeader('Access-Control-Allow-Credentials', "true");
--- utils/decrypt.js
+++ utils/decrypt.js
@@ -1,51 +1,51 @@
-// const CryptoJS = require('crypto-js')
+const CryptoJS = require('crypto-js')
 
-// class WebEncryption {
-//     #base64Key;
-//     #plainKey;
+class WebEncryption {
+    #base64Key;
+    #plainKey;
 
-//     constructor(key) {
-//         this.#base64Key = CryptoJS.enc.Base64.parse(key);
-//         this.#plainKey = key;
-//     }
+    constructor(key) {
+        this.#base64Key = CryptoJS.enc.Base64.parse(key);
+        this.#plainKey = key;
+    }
 
-//     decrypt(crypted_data) {
-//         const bytes = Buffer.from(crypted_data, "base64");
-//         const str = bytes.toString();
+    decrypt(crypted_data) {
+        const bytes = Buffer.from(crypted_data, "base64");
+        const str = bytes.toString();
 
-//         if (str.startsWith("Salted")) {
-//             let bytes = CryptoJS.AES.decrypt(crypted_data, this.#plainKey);
-//             let originalText = bytes.toString(CryptoJS.enc.Utf8);
+        if (str.startsWith("Salted")) {
+            let bytes = CryptoJS.AES.decrypt(crypted_data, this.#plainKey);
+            let originalText = bytes.toString(CryptoJS.enc.Utf8);
 
-//             return originalText;
-//         } else {
-//             const data = JSON.parse(str);
-//             const iv = CryptoJS.enc.Base64.parse(data.iv);
-//             const value = CryptoJS.enc.Base64.parse(data.value);
-//             const decrypted = CryptoJS.AES.decrypt(
-//                 { ciphertext: value },
-//                 this.#base64Key,
-//                 {
-//                     iv: iv,
-//                     mode: CryptoJS.mode.CBC,
-//                 }
-//             );
-//             const decrypted_text = decrypted.toString(CryptoJS.enc.Utf8);
+            return originalText;
+        } else {
+            const data = JSON.parse(str);
+            const iv = CryptoJS.enc.Base64.parse(data.iv);
+            const value = CryptoJS.enc.Base64.parse(data.value);
+            const decrypted = CryptoJS.AES.decrypt(
+                { ciphertext: value },
+                this.#base64Key,
+                {
+                    iv: iv,
+                    mode: CryptoJS.mode.CBC,
+                }
+            );
+            const decrypted_text = decrypted.toString(CryptoJS.enc.Utf8);
 
-//             return decrypted_text;
-//         }
-//     }
+            return decrypted_text;
+        }
+    }
 
-//     encrypt(data) {
-//         const encryptedData = CryptoJS.AES.encrypt(
-//             data.toString(),
-//             this.#plainKey
-//         ).toString();
+    encrypt(data) {
+        const encryptedData = CryptoJS.AES.encrypt(
+            data.toString(),
+            this.#plainKey
+        ).toString();
 
-//         return encryptedData;
-//     }
-// }
+        return encryptedData;
+    }
+}
 
 
-// module.exports = WebEncryption
+module.exports = WebEncryption
 
