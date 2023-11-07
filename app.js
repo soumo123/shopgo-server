b2b9a0a94786410e4ab315bc067c41d97e4c7974
Soummya Biswas <soummyabiswas999@gmail.com>
2023-11-07 22:04:18 +0530

    changes

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST , OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
